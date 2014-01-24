4a6be9326a40e6254e073ecf87b4001fb25c0489
Brian Gyss <brian.gyss@gmail.com>
2014-01-24 17:44:44 -0500

    Refactored AdAccount, AdCampaign, AdCreative, AdGroup models to remove TinyModel dependency

--- pyfacebook/models.py
+++ pyfacebook/models.py
@@ -2,7 +2,10 @@ import random
 import pytz
 import datetime
 import calendar
+import json
+from dateutil import parser as date_parser
 from tinymodel import TinyModel, FieldDef
+from collections import namedtuple
 
 unix_datetime_translators = {
     'to_json': lambda obj: calendar.timegm(datetime.datetime.utcfromtimestamp(obj.utctimetuple())),
@@ -302,128 +305,240 @@ class Preview(FacebookModel):
         FieldDef(title='body', allowed_types=[unicode]),
     ]
 
+NewFieldDef = namedtuple('NewFieldDef', ['title', 'allowed_types', 'choices'])
 
-class AdCreative(FacebookModel):
+
+class AdBase(object):
+    """
+    Common class for AdAccount, AdCampaign, AdCreative, AdGroup to simulate TinyModel.
+    """
+    def __repr__(self):
+        frepr = {}
+        for x in self.FIELDS:
+            frepr[x['field_def'].title] = x['value']
+        return str(self.__class__) + "\nFIELDS:\n" + str(frepr) + "\n"
+
+    def __getattr__(self, name):
+        fields = object.__getattribute__(self, 'FIELDS')
+        f = next((x for x in fields if x['field_def'].title == name), None)
+        if f:
+            return f['value']
+        else:
+            raise AttributeError(str(self.__class__) + " has no field " + name)
+
+    def __setattr__(self, key, value):
+        field_def = next((x for x in self.FIELD_DEFS if x.title == key), False)
+        if field_def:
+            if type(value) in [str, unicode, int, long] and datetime.datetime in field_def.allowed_types:
+                if type(value) in [int, long]:
+                    value = datetime.datetime.fromtimestamp(value)
+                elif type(value) in [str, unicode]:
+                    value = date_parser.parser(value)
+                else:
+                    raise ValueError
+            if self.__validate(value, field_def.allowed_types):
+                if 'choices' in field_def:
+                    if self.__checkchoices(value, field_def.choices):
+                        self.__add_field(field_def, value)
+                    else:
+                        e = "{0} is not present in choices {1}".format(value, field_def.choices)
+                        raise ValueError(e)
+                else:
+                    self.__add_field(field_def, value)
+            else:
+                e = "{0} does not validate against allowed_types {1}"\
+                    .format(value, field_def.allowed_types)
+                raise ValueError(e)
+
+    def __init__(self, from_json=False, **kwargs):
+        object.__setattr__(self, 'FIELDS', [])
+        if from_json:
+            initial_attrib = self.__from_json(from_json)
+        else:
+            initial_attrib = kwargs
+
+        for (key, value) in initial_attrib.items():
+            setattr(self, key, value)
+
+    def __from_json(self, json_data):
+        return json.loads(json_data)
+
+    def __checkchoices(self, value, choices):
+        if value in choices:
+            return True
+
+    def __add_field(self, field_def, value):
+        this_field = next((x for x in self.FIELDS if x['field_def'].title == field_def.title), None)
+        if not this_field:
+            self.FIELDS.append({'field_def': field_def, 'value': value})
+        else:
+            this_field['value'] = value
+
+    def __validate(self, value, allowed_types):
+
+        if long in allowed_types:
+            allowed_types.append(int)
+
+        if isinstance(value, str):
+            value = unicode(value)
+
+        if isinstance(value, dict):
+            new_val = {}
+            for k, v in value.iteritems():
+                if type(k) == str:
+                    k = unicode(k)
+                elif type(v) == str:
+                    v = unicode(v)
+                new_val[k] = v
+            dict_validator = [x for x in allowed_types if type(x) == dict][0]
+            key_type = dict_validator.keys()[0]
+            val_type = dict_validator.values()[0]
+            if key_type == type(new_val.keys()[0]) and val_type == type(new_val.values()[0]):
+                return True
+
+        if isinstance(allowed_types, list):
+            if len(allowed_types) > 1:
+                if type(value) in tuple(allowed_types):
+                    return True
+            else:
+                if not isinstance(allowed_types[0], list):
+                    if isinstance(value, allowed_types[0]):
+                        return True
+                else:
+                    whole_list_validator = []
+                    for v in value:
+                        if isinstance(v, allowed_types[0][0]):
+                            whole_list_validator.append(True)
+                        else:
+                            whole_list_validator.append(False)
+                    if True in whole_list_validator:
+                        return True
+
+
+class AdCreative(AdBase):
 
     """
     Represents the adcreative object in the Facebook Ads API:
     https://developers.facebook.com/docs/reference/ads-api/adcreative
 
     """
-    FIELD_DEFS = [
-        FieldDef(title='id', allowed_types=[long]),
-        FieldDef(title='type', allowed_types=[int], choices=[1, 2, 3, 4, 12, 25, 27]),
-        FieldDef(title='object_id', allowed_types=[long]),
-        FieldDef(title='name', allowed_types=[unicode]),
-        FieldDef(title='title', allowed_types=[unicode]),
-        FieldDef(title='body', allowed_types=[unicode]),
-        FieldDef(title='image_hash', allowed_types=[unicode, type(None)]),
-        FieldDef(title='image_url', allowed_types=[unicode]),
-        FieldDef(title='link_url', allowed_types=[unicode]),
-        FieldDef(title='preview_url', allowed_types=[unicode]),
-        FieldDef(title='url_tags', allowed_types=[unicode]),
-        FieldDef(title='related_fan_page', allowed_types=[long]),
-        FieldDef(title='story_id', allowed_types=[long]),
-        FieldDef(title='follow_redirect', allowed_types=[bool]),
-        FieldDef(title='auto_update', allowed_types=[bool]),
-        FieldDef(title='action_spec', allowed_types=[[ActionSpec]]),
-        FieldDef(title='previews', allowed_types=[[Preview]]),
-    ]
+    FIELD_DEFS = (
+        NewFieldDef(title='id', allowed_types=[long], choices=None),
+        NewFieldDef(title='type', allowed_types=[int], choices=[1, 2, 3, 4, 12, 25, 27]),
+        NewFieldDef(title='object_id', allowed_types=[long], choices=None),
+        NewFieldDef(title='name', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='title', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='body', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='image_hash', allowed_types=[unicode, type(None)], choices=None),
+        NewFieldDef(title='image_url', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='link_url', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='preview_url', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='url_tags', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='related_fan_page', allowed_types=[long], choices=None),
+        NewFieldDef(title='story_id', allowed_types=[long], choices=None),
+        NewFieldDef(title='follow_redirect', allowed_types=[bool], choices=None),
+        NewFieldDef(title='auto_update', allowed_types=[bool], choices=None),
+        NewFieldDef(title='action_spec', allowed_types=[[ActionSpec]], choices=None),
+        NewFieldDef(title='previews', allowed_types=[[Preview]], choices=None),
+    )
 
-    CREATE_ONLY = ['follow_redirect']
-    CONNECTIONS = ['previews']
+    def __init__(self, from_json=False, **kwargs):
+        super(AdCreative, self).__init__(from_json, **kwargs)
 
 
-class AdGroup(FacebookModel):
+class AdGroup(AdBase):
 
     """
     Represents an adgroup object in the Facebook Ads API:
     https://developers.facebook.com/docs/reference/ads-api/adgroup
 
     """
-    FIELD_DEFS = [
-        FieldDef(title='id', allowed_types=[long]),
-        FieldDef(title='name', allowed_types=[unicode]),
-        FieldDef(title='account_id', allowed_types=[int]),
-        FieldDef(title='campaign_id', allowed_types=[long]),
-        FieldDef(title='adgroup_status', allowed_types=[unicode], choices=['ACTIVE', 'DELETED', 'PENDING_REVIEW', 'DISAPPROVED', 'PENDING_BILLING_INFO', 'CAMPAIGN_PAUSED', 'ADGROUP_PAUSED']),
-        FieldDef(title='disapprove_reason_descriptions', allowed_types=[unicode]),
-        FieldDef(title='bid_type', allowed_types=[unicode], choices=['CPC', 'CPM', 'MULTI_PREMIUM', 'RELATIVE_OCPM', 'ABSOLUTE_OCPM', 'CPA']),
-        FieldDef(title='bid_info', allowed_types=[{unicode: int}, type(None)]),
-        FieldDef(title='creative_ids', allowed_types=[[long]]),
-        FieldDef(title='creative', allowed_types=[{unicode: long}]),
-        FieldDef(title='targeting', allowed_types=[Targeting, type(None)]),
-        FieldDef(title='tracking_specs', allowed_types=[[TrackingSpec]]),
-        FieldDef(title='last_updated_by_app_id', allowed_types=[long]),
-        FieldDef(title='created_time', allowed_types=[datetime.datetime]),
-        FieldDef(title='updated_time', allowed_types=[datetime.datetime]),
-        FieldDef(title='stats', allowed_types=[[AdStatistic]]),
-        FieldDef(title='adcreatives', allowed_types=[[AdCreative]]),
-        FieldDef(title='previews', allowed_types=[[Preview]]),
-    ]
+    FIELD_DEFS = (
+        NewFieldDef(title='id', allowed_types=[long], choices=None),
+        NewFieldDef(title='name', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='account_id', allowed_types=[int], choices=None),
+        NewFieldDef(title='campaign_id', allowed_types=[long], choices=None),
+        NewFieldDef(title='adgroup_status', allowed_types=[unicode],
+                    choices=['ACTIVE', 'DELETED', 'PENDING_REVIEW', 'DISAPPROVED',
+                             'PENDING_BILLING_INFO', 'CAMPAIGN_PAUSED', 'ADGROUP_PAUSED']),
+        NewFieldDef(title='disapprove_reason_descriptions', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='bid_type', allowed_types=[unicode],
+                    choices=['CPC', 'CPM', 'MULTI_PREMIUM', 'RELATIVE_OCPM', 'ABSOLUTE_OCPM', 'CPA']),
+        NewFieldDef(title='bid_info', allowed_types=[{unicode: int}, type(None)], choices=None),
+        NewFieldDef(title='creative_ids', allowed_types=[[long]], choices=None),
+        NewFieldDef(title='creative', allowed_types=[{unicode: long}], choices=None),
+        NewFieldDef(title='targeting', allowed_types=[Targeting, type(None)], choices=None),
+        NewFieldDef(title='tracking_specs', allowed_types=[[TrackingSpec]], choices=None),
+        NewFieldDef(title='last_updated_by_app_id', allowed_types=[long], choices=None),
+        NewFieldDef(title='created_time', allowed_types=[datetime.datetime], choices=None),
+        NewFieldDef(title='updated_time', allowed_types=[datetime.datetime], choices=None),
+        NewFieldDef(title='stats', allowed_types=[[AdStatistic]], choices=None),
+        NewFieldDef(title='adcreatives', allowed_types=[[AdCreative]], choices=None),
+        NewFieldDef(title='previews', allowed_types=[[Preview]], choices=None),
+    )
 
-    CREATE_ONLY = ['creative']
-    CONNECTIONS = ['stats', 'adcreatives', 'previews']
+    def __init__(self, from_json=False, **kwargs):
+        super(AdGroup, self).__init__(from_json, **kwargs)
 
 
-class AdCampaign(FacebookModel):
+class AdCampaign(AdBase):
 
     """
     Represents the aduser object in the Facebook Ads API:
     https://developers.facebook.com/docs/reference/ads-api/adcampaign/
 
     """
-    FIELD_DEFS = [
-        FieldDef(title='id', allowed_types=[long]),
-        FieldDef(title='name', allowed_types=[unicode]),
-        FieldDef(title='account_id', allowed_types=[long]),
-        FieldDef(title='start_time', allowed_types=[datetime.datetime]),
-        FieldDef(title='end_time', allowed_types=[datetime.datetime]),
-        FieldDef(title='created_time', allowed_types=[datetime.datetime]),
-        FieldDef(title='updated_time', allowed_types=[datetime.datetime]),
-        FieldDef(title='daily_budget', allowed_types=[int]),
-        FieldDef(title='lifetime_budget', allowed_types=[int]),
-        FieldDef(title='budget_remaining', allowed_types=[int]),
-        FieldDef(title='campaign_status', allowed_types=[int], choices=[1, 2, 3]),
-        FieldDef(title='adcreatives', allowed_types=[[AdCreative]]),
-        FieldDef(title='adgroups', allowed_types=[[AdGroup]]),
-        FieldDef(title='stats', allowed_types=[[AdStatistic]]),
-    ]
+    FIELD_DEFS = (
+        NewFieldDef(title='id', allowed_types=[long], choices=None),
+        NewFieldDef(title='name', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='account_id', allowed_types=[long], choices=None),
+        NewFieldDef(title='start_time', allowed_types=[datetime.datetime], choices=None),
+        NewFieldDef(title='end_time', allowed_types=[datetime.datetime], choices=None),
+        NewFieldDef(title='created_time', allowed_types=[datetime.datetime], choices=None),
+        NewFieldDef(title='updated_time', allowed_types=[datetime.datetime], choices=None),
+        NewFieldDef(title='daily_budget', allowed_types=[int], choices=None),
+        NewFieldDef(title='lifetime_budget', allowed_types=[int], choices=None),
+        NewFieldDef(title='budget_remaining', allowed_types=[int], choices=None),
+        NewFieldDef(title='campaign_status', allowed_types=[int], choices=[1, 2, 3]),
+        NewFieldDef(title='adcreatives', allowed_types=[[AdCreative]], choices=None),
+        NewFieldDef(title='adgroups', allowed_types=[[AdGroup]], choices=None),
+        NewFieldDef(title='stats', allowed_types=[[AdStatistic]], choices=None),
+    )
 
-    CONNECTIONS = ['adcreatives', 'adgroups', 'stats']
+    def __init__(self, from_json=False, **kwargs):
+        super(AdCampaign, self).__init__(from_json, **kwargs)
 
 
-class AdAccount(FacebookModel):
-
+class AdAccount(AdBase):
     """
     Represents the adaccount object in the Facebook Ads API:
     https://developers.facebook.com/docs/reference/ads-api/adaccount/
 
     """
-    FIELD_DEFS = [
-        FieldDef(title='id', allowed_types=[unicode]),
-        FieldDef(title='account_id', allowed_types=[long]),
-        FieldDef(title='name', allowed_types=[unicode]),
-        FieldDef(title='account_status', allowed_types=[int]),
-        FieldDef(title='currency', allowed_types=[unicode]),
-        FieldDef(title='timezone_id', allowed_types=[int]),
-        FieldDef(title='timezone_name', allowed_types=[unicode]),
-        FieldDef(title='timezone_offset_hours_utc', allowed_types=[int]),
-        FieldDef(title='vat_status', allowed_types=[int]),
-        FieldDef(title='daily_spend_limit', allowed_types=[int]),
-        FieldDef(title='amount_spent', allowed_types=[int]),
-        FieldDef(title='users', allowed_types=[[AdUser]]),
-        FieldDef(title='adcampaigns', allowed_types=[[AdCampaign]]),
-        FieldDef(title='adimages', allowed_types=[[AdImage]]),
-        FieldDef(title='adcreatives', allowed_types=[[AdCreative]]),
-        FieldDef(title='adgroups', allowed_types=[[AdGroup]]),
-        FieldDef(title='stats', allowed_types=[[AdStatistic]]),
-        FieldDef(title='adgroupstats', allowed_types=[[AdStatistic]]),
-        FieldDef(title='adpreviewscss', allowed_types=[[AdPreviewCss]]),
-    ]
+    FIELD_DEFS = (
+        NewFieldDef(title='id', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='account_id', allowed_types=[long], choices=None),
+        NewFieldDef(title='name', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='account_status', allowed_types=[int], choices=None),
+        NewFieldDef(title='currency', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='timezone_id', allowed_types=[int], choices=None),
+        NewFieldDef(title='timezone_name', allowed_types=[unicode], choices=None),
+        NewFieldDef(title='timezone_offset_hours_utc', allowed_types=[int], choices=None),
+        NewFieldDef(title='vat_status', allowed_types=[int], choices=None),
+        NewFieldDef(title='daily_spend_limit', allowed_types=[int], choices=None),
+        NewFieldDef(title='amount_spent', allowed_types=[int], choices=None),
+        NewFieldDef(title='users', allowed_types=[[AdUser]], choices=None),
+        NewFieldDef(title='adcampaigns', allowed_types=[[AdCampaign]], choices=None),
+        NewFieldDef(title='adimages', allowed_types=[[AdImage]], choices=None),
+        NewFieldDef(title='adcreatives', allowed_types=[[AdCreative]], choices=None),
+        NewFieldDef(title='adgroups', allowed_types=[[AdGroup]], choices=None),
+        NewFieldDef(title='stats', allowed_types=[[AdStatistic]], choices=None),
+        NewFieldDef(title='adgroupstats', allowed_types=[[AdStatistic]], choices=None),
+        NewFieldDef(title='adpreviewscss', allowed_types=[[AdPreviewCss]], choices=None),
+    )
 
-    CONNECTIONS = ['users', 'adcampaigns', 'adimages', 'adcreatives',
-                   'adgroups', 'stats', 'adgroupstats', 'adpreviewscss']
+    def __init__(self, from_json=False, **kwargs):
+        super(AdAccount, self).__init__(from_json, **kwargs)
 
 
 class Post(FacebookModel):
